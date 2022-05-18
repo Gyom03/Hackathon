1e08da1217ede863e275e06b4a68615c3486e5e7
Frekii <jeremy.opitz@std.heh.be>
2022-05-18 18:39:09 +0200

    Création classes Colis et Etagère

---
+++ Hpost/Hpost/Étagère.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hpost
+{
+    internal class Étagère
+    {
+        private int PoidsMax;
+        private int VolumeMax;
+        private int Colonne;
+        private int Ligne;
+
+        public int GetLigne()
+        {
+            return GetLigne();
+        }
+
+        public void SetLigne(int value)
+        {
+            SetLigne(value);
+        }
+
+        public int GetColonne()
+        {
+            return GetColonne();
+        }
+
+        public void SetColonne(int value)
+        {
+            SetColonne(value);
+        }
+
+        public int GetVolumeMax()
+        {
+            return GetVolumeMax();
+        }
+
+        public void SetVolumeMax(int value)
+        {
+            SetVolumeMax(value);
+        }
+
+        public int GetPoidsMax()
+        {
+            return GetPoidsMax();
+        }
+
+        public void SetPoidsMax(int value)
+        {
+            SetPoidsMax(value);
+        }
+    }
+}
