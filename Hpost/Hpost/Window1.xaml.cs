720357fd6bf52c2ff513a94fe261095ca06373a5
rosind <99585997+rosind@users.noreply.github.com>
2022-05-18 20:42:16 +0200

    dd

--- Hpost/Hpost/Window1.xaml.cs
+++ Hpost/Hpost/Window1.xaml.cs
@@ -10,21 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Hpost
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-            Apropos apropos = new Apropos();
-            apropos.Show();
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
